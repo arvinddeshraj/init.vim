73cd768dcdcdd9b683f8196011e76b97716d4d78
Arvind Deshraj <arvind.deshraj@gmail.com>
2022-10-11 09:31:55 +0530

    add folke/noice
    Saw this on Twitter so I'm testing this out. Looks pretty insane, let's
    see if I keep this long-term

--- lua/trinity/packer.lua
+++ lua/trinity/packer.lua
@@ -32,5 +32,18 @@ return require('packer').startup(function()
     use 'kyazdani42/nvim-web-devicons'
     use 'hoob3rt/lualine.nvim'
     use 'rebelot/kanagawa.nvim'
+    use({
+      "folke/noice.nvim",
+      event = "VimEnter",
+      config = function()
+        require("noice").setup()
+      end,
+      requires = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+        }
+    })
+
 end)
 
