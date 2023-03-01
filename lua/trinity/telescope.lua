e636350e080cf17e09bc746816ad04534a1ebdaf
Arvind Deshraj <arvind.deshraj@gmail.com>
2023-03-01 16:48:48 +0530

    add current buffer fuzzy search
    nvim-lua/kickstart.nvim is filled with gems. Wouldn't expect any less
    from TJ. Will probably spend some time and go through it end to end

--- lua/trinity/telescope.lua
+++ lua/trinity/telescope.lua
@@ -12,3 +12,9 @@ vim.keymap.set("n", "<Leader>ff", "<cmd>lua require('telescope.builtin').find_fi
 vim.keymap.set("n", "<Leader>rg", "<cmd>lua require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown({}))<cr>", { noremap=true })
 vim.keymap.set("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown({}))<cr>", { noremap=true })
 vim.keymap.set("n", "<c-p>", "<cmd>lua require('telescope.builtin').git_files(require('telescope.themes').get_dropdown({}))<cr>", { noremap=true })
+vim.keymap.set("n", "<Leader>/", function()
+    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end)
