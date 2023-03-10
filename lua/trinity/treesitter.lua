22398c8ff02cadb77e7d55d4e0ff7d6acd00700b
Arvind Deshraj <arvind.deshraj@gmail.com>
2023-03-10 10:40:49 +0530

    remove trailing whitespace in treesitter config

--- lua/trinity/treesitter.lua
+++ lua/trinity/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
     ensure_installed = {"c", "cpp", "rust", "python"},
-    -- Install languages synchronously (only applied to `ensure_installed`)                                                        
+    -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
     -- List of parsers to ignore installing
     ignore_install = {},
