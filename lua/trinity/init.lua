b09ba3a8f306edc07bcc6e73452954560454fb97
Arvind Deshraj <arvind.deshraj@gmail.com>
2023-11-16 14:33:13 +0530

    expandtab is back
    Tried to use tabs by default but I just don't like it, it's not
    consistent everywhere, it's quite annoying particularly online so I'm
    going back to spaces

--- lua/trinity/init.lua
+++ lua/trinity/init.lua
@@ -40,6 +40,7 @@ vim.opt.listchars = {
 vim.g.mapleader = " " -- space is the leader
 
 -- Indentation
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
