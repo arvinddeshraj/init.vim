605f71f59b15c7c7fa960096e8e03143a6a50ccf
Arvind <arvind.deshraj@gmail.com>
2026-04-14 16:06:14 +0530

    add jj repo for telescope git files
    <space>gf will now support listing vsc tracked files in both git and jj

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -27,7 +27,31 @@ return {
                 cwd = vim.fn.stdpath('config')
             }
         end)
-        vim.keymap.set("n", "<space>gf", require("telescope.builtin").git_files)
+
+        local function vcs_files()
+            local builtin = require("telescope.builtin")
+
+            -- first check if we're in a git repo
+            vim.fn.system("git rev-parse --is-inside-work-tree")
+            if vim.v.shell_error == 0 then
+                builtin.git_files()
+                return
+            end
+
+            -- check if we're in a jj repo
+            vim.fn.system("jj root")
+            if vim.v.shell_error == 0 then
+                builtin.find_files({
+                    find_command = {"jj", "file", "list"},
+                })
+                return
+            end
+
+            -- if it's neither git nor jj repo
+            vim.notify("Not in a git or jj repository", vim.log.levels.ERROR)
+        end
+
+        vim.keymap.set("n", "<space>gf", vcs_files)
         vim.keymap.set("n", "<space>rg", require("telescope.builtin").live_grep)
     end
 }
